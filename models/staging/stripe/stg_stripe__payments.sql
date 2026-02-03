d09e19f833ec3d119a9c9198fa32085571371310
Brian O'Connor <estoyboc@gmail.com>
2026-02-03 16:48:43 +0000

    added jaffle_shop and stripe source yaml files.

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,10 @@
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    _batched_at as created_at,
+
+from raw.stripe.payment
